a08d770cb94d325e91d30fe83609cf008b8a84bc
mon5500000 <monthanapat30@gmail.com>
2024-11-10 18:17:42 +0700

    Update IMPOSSIBLE-Obby-to-Heaven.lua

--- IMPOSSIBLE-Obby-to-Heaven.lua
+++ IMPOSSIBLE-Obby-to-Heaven.lua
@@ -1,3 +1,2 @@
--- This file was generated using Luraph Obfuscator v14.3 [https://lura.ph/]
 --https://www.roblox.com/th/games/15919221063
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/unahub/refs/heads/main/source/IMPOSSIBLE-Obby-to-Heaven.lua"))()
