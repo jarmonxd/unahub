5f34c450af03d2d8a5a63deca3ab76dedbb4037c
mon5500000 <monthanapat30@gmail.com>
2024-11-12 22:06:18 +0700

    Update Really-Easy-Obby.lua

---
+++ Really-Easy-Obby.lua
@@ -0,0 +1,3 @@
+--https://www.roblox.com/th/games/8472851459/Really-Easy-Obby
+_G.delaytime = 0.2 -- ดีเลย์เวลาวาป
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/unahub/refs/heads/main/source/Really-Easy-Obby.lua"))()
