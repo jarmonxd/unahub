4e3b614128e2269ae4c5320c0e66b4f330e4de93
mon5500000 <monthanapat30@gmail.com>
2025-02-18 19:35:52 +0700

    เพิ่มไฟล์สําหรับคนไม่อยากใช้ loader

---
+++ IMPOSSIBLE-OBBY.lua
@@ -0,0 +1,3 @@
+--https://www.roblox.com/th/games/16247300990
+_G.delaytime = 0.10 -- ดีเลย์เวลาวาป
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jarmonxd/unahub/refs/heads/main/source/IMPOSSIBLE-OBBY.lua"))()
